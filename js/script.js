2094099084aab62bd3d16bd76eae8316a95b0fa8
Pietro Donelli <pietro.donelli99@gmail.com>
2020-09-24 09:04:47 +0200

    Slider verticale

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-	//...
+	var swiper = new Swiper("#vertical-slider", {
+		direction: "vertical",
+		//mousewheel: true,
+		keyboard: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+	});
 });
