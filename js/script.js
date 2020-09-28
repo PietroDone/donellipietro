12e9c2e571bb0975bac4d43bd14542944f552053
Pietro Donelli <pietro.donelli99@gmail.com>
2020-09-28 08:17:32 +0200

    Aggiunte immagini header e gestione in responsive

--- js/script.js
+++ js/script.js
@@ -11,6 +11,17 @@ $(document).ready(function () {
 
 	var barraMenu = $(".main-menu");
 	$(window).on("load resize scroll", function () {
+		/* Chiusura menu al resize */
+		chiudiMenu();
+
+		/* Gestione apparizione menu */
+		if (screen && screen.width >= 768) {
+			$(".main-menu .inline-menu").slideDown();
+		} else {
+			$(".main-menu .inline-menu").slideUp();
+		}
+
+		/* Gestione fissaggio barra */
 		var scroll = $(window).scrollTop();
 		if (screen && screen.width > 200) {
 			if (scroll >= 20) {
@@ -21,11 +32,28 @@ $(document).ready(function () {
 		}
 	});
 
+	var pietro = $(".full-hero .image img");
+	var w_p = pietro.width();
+	var h_p = pietro.height();
+
+	$(window).on("load resize scroll", function () {
+		image = $(".full-hero .image");
+		w_i = image.width();
+		h_i = image.height();
+
+		if ((h_p / w_p) * w_i < h_i) {
+			pietro.css("width", w_i + "px").css("height", "auto");
+			image.css("border-color", "green");
+		} else {
+			pietro.css("height", h_i + "px").css("width", "auto");
+			image.css("border-color", "blue");
+		}
+	});
+
 	// Gestione shadow menu
 	function addShadow() {
 		var bodyShadow = document.createElement("div");
 		bodyShadow.setAttribute("class", "body-shadow");
-		console.log(bodyShadow);
 		$("body header").append(bodyShadow);
 		$(".body-shadow").fadeIn();
 	}
@@ -45,7 +73,9 @@ $(document).ready(function () {
 		removeShadow();
 		$(".hamburger").removeClass("is-active");
 		$(".barra-menu").removeClass("menu-open");
-		$(".main-menu .inline-menu").slideUp();
+		if (screen && screen.width < 768) {
+			$(".main-menu .inline-menu").slideUp();
+		}
 	}
 
 	// Trigger apertura e chiusura
