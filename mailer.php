196bd8e281769881f7eb83f8e44934790604cc5f
Pietro Donelli <pietro.donelli99@gmail.com>
2020-10-11 16:51:33 +0200

    Aggiornata mail form

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "pietro.donelli99@gmail.com";
+        $recipient = "info@donellipietro.it";
 
         // Set the email subject.
         $subject = "Nuovo contatto da $name";
